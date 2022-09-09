346ea0171a6e377bcf7af30e113132d91c64218d
Mithil Hassan <mithilhassan007@gmail.com>
2022-09-09 23:27:07 +0600

    javascript added

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const handleColor = () => {
+  let index = Math.floor(Math.random() * colors.length);
+  let randomColor = colors[index];
+  document.querySelector(".color").innerText = randomColor;
+  document.body.style.background = randomColor;
+};
--- hex.js
+++ hex.js
@@ -1,1 +1,12 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const generateHex = () => {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * 16);
+    let value = hex[index];
+    hexColor += value;
+  }
+  document.querySelector(".color").innerText = hexColor;
+  document.body.style.background = hexColor;
+};
